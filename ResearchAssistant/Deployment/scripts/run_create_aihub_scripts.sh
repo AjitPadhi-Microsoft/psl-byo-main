d9e9c779af84cae7d9ec74b1b3c0a7bba5eb649f
Ajit Padhi <v-padhiajit@microsoft.com>
2024-11-11 22:40:29 +0530

    updated ai hub code

--- ResearchAssistant/Deployment/scripts/run_create_aihub_scripts.sh
+++ ResearchAssistant/Deployment/scripts/run_create_aihub_scripts.sh
@@ -40,6 +40,10 @@ sed -i "s/rg_to-be-replaced/${resourceGroupName}/g" "create_ai_hub.py"
 sed -i "s/solutionname_to-be-replaced/${solutionName}/g" "create_ai_hub.py"
 sed -i "s/solutionlocation_to-be-replaced/${solutionLocation}/g" "create_ai_hub.py"
 
+# Create and activate a virtual environment
+python -m venv myenv
+source myenv/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
 
 python create_ai_hub.py
